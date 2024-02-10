d946861738f462248207ff1009da2e84f2e344fc
Olga Zhivaeva <olga.zhiv@yandex.ru>
2024-02-10 18:42:12 +0300

    Added improvements

---
+++ README.md
@@ -0,0 +1,25 @@
+# Bitly URL shortener
+
+Use the app to create short links and track them
+
+### How to install
+
+Register on the website [Bitli](https://app.bitly.com/Bo1vhyBIxse/home)
+
+Generate a token [GENERIC ACCESS TOKEN](https://app.bitly.com/settings/api)
+
+Store the token in the `BITLY_TOKEN` variable in the `.env` file:
+```
+BITLY_TOKEN="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+```
+
+
+Python3 should already be installed. 
+Use `pip` (or `pip3`, if there is a conflict with Python2) to install dependencies:
+```
+pip install -r requirements.txt
+```
+
+### Project Goals
+
+This code was written for educational purposes as part of an online course for web developers at [dvmn.org](https://dvmn.org/).
--- main.py
+++ main.py
@@ -1,74 +1,56 @@
 import os
 import requests
-from pprint import pprint
 from urllib.parse import urlparse
 from dotenv import load_dotenv
+
+
 HOST = "https://api-ssl.bitly.com"
+
 
 def shorten_link(token, link):
     """Функция сокращает ссылку"""
     url = f"{HOST}/v4/shorten"
-    json = {"long_url": user_url}
-    response = requests.post(url, headers=headers, json=json)
+    content = {"long_url": user_url}
+    response = requests.post(url, headers=headers, json=content)
     response.raise_for_status()
     return response.json()["link"]
+
 
 def count_clicks(token, link):
     """Функция считает клики"""
     bitlink = f"bit.ly{urlparse(link).path}"
     url = f"{HOST}/v4/bitlinks/{bitlink}/clicks/summary"
-    params = {"unit": "day","units": -1}
+    params = {"unit": "day", "units": -1}
     response = requests.get(url, params=params, headers=headers)
-    # print(response.status_code)
-    # print(response.json())
     response.raise_for_status()
     return response.json()["total_clicks"]
 
+
 def is_bitlink(link):
-    """Функция проверяет является ли ссылка битлинком """
+    """Функция проверяет является ли ссылка битлинком"""
+    netloc = urlparse(link).netloc
     path = urlparse(link).path
-    url = f"{HOST}/v4/bitlinks/bit.ly{path}"
+    url = f"{HOST}/v4/bitlinks/{netloc}/{path}"
     response = requests.get(url, headers=headers)
     return response.ok
 
-def get_profile(token):
-    """"Функция дает сведения об аккаунте"""
-    url = f"{HOST}/v4/user"
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    return response.json()
 
-def main():
-    """Основная функция"""
+if __name__ == "__main__":
+    load_dotenv()
+    token = os.environ["BITLY_TOKEN"]
+    headers = {"Authorization": f"Bearer {token}"}
+    user_url = input("Введите ссылку: ")
     if is_bitlink(user_url):
         try:
             count = count_clicks(token, user_url)
         except requests.exceptions.HTTPError:
             print("Что-то пошло не так")
         else:
-            print(f"Число кликов: {count}")
+            print(f"По вашей ссылке прошли {count} раз(а)")
     else:
         try:
             bitlink = shorten_link(token, user_url)
         except requests.exceptions.HTTPError:
             print("Вы ввели не верную ссылку")
         else:
-            print(f"Короткая ссылка: {bitlink}")
-
-if __name__ == "__main__":
-    load_dotenv()
-    token = os.getenv("TOKEN")
-    headers = {"Authorization": f"Bearer {token}"}
-    # user_url = "https://devops.org.ru/docker-summary#d1"
-
-    try:
-        profile = get_profile(token)
-    except requests.exceptions.HTTPError:
-        print("Токен не работает")
-    else:
-        print(f"Ваш профиль:")
-        pprint(profile)
-        user_url = input("Введите ссылку: ")
-        main()
-
-
+            print(f"Битлинк: {bitlink}")
